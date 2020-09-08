5800a345c71e8b132cf65f28907a15bee888ef5f
Alonso Avalo <alonso_avalo97@hotmail.com>
2020-09-07 19:40:03 -0500

    Test

---
+++ Assets/Scripts/gyroMove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroMove : MonoBehaviour
+{
+    private void Start() {
+    #if UNITY_ANDROID
+        QualitySettings.vSyncCount = 0; 
+        Application.targetFrameRate = 60; 
+        QualitySettings.antiAliasing = 0; 
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    #endif
+    }
+     private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            Vector2 touchDelta = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDelta.x,-touchDelta.y,0);
+        }
+            
+    }
+}
